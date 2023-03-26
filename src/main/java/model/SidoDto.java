7a9235f3f2eef054abb924089510ceb578fd4dd4
skydreamer21 <skydreamer210@gmail.com>
2023-03-26 20:43:43 +0900

    feat: SidoDto 생성

---
+++ src/main/java/model/SidoDto.java
@@ -0,0 +1,28 @@
+package model;
+
+public class SidoDto {
+	private int sidoCode;
+	private String sidoName;
+	
+	public SidoDto(int sidoCode, String sidoName) {
+		super();
+		this.sidoCode = sidoCode;
+		this.sidoName = sidoName;
+	}
+
+	public int getSidoCode() {
+		return sidoCode;
+	}
+
+	public void setSidoCode(int sidoCode) {
+		this.sidoCode = sidoCode;
+	}
+
+	public String getSidoName() {
+		return sidoName;
+	}
+
+	public void setSidoName(String sidoName) {
+		this.sidoName = sidoName;
+	}
+}
