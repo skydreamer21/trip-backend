c160bd858181b41de12f5401cbcda54b4d4b70e0
skydreamer21 <skydreamer210@gmail.com>
2023-03-27 08:41:11 +0900

    feat: BoardDao 구현(미완)

---
+++ src/main/java/model/BoardDaoImpl.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.List;
+
+public class BoardDaoImpl implements iBoardDao {
+
+	@Override
+	public List<BoardDto> selectAllBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardDto selectByArticleNo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
