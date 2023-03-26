f62736057c9ee2d61752f800ee165a8d50909db6
skydreamer21 <skydreamer210@gmail.com>
2023-03-26 18:39:22 +0900

    fix: dbutil url 수정

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -9,7 +9,8 @@ import java.sql.SQLException;
 public class DBUtil {
 	
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost:3306/ssafyweb?serverTimezone=UTC";
+	private static final String DB_SCHEMAS = "ssafytrip"; 
+	private static final String URL = "jdbc:mysql://localhost:3306/" + DB_SCHEMAS + "?serverTimezone=UTC";
 	private static final String DB_ID = "ssafy";
 	private static final String DB_PWD = "ssafy";
 	
