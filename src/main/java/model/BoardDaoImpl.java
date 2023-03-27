22a32e2f42a98e6b0092e0cef0ef39bb0ff57200
김주현 <skydreamer21@naver.com>
2023-03-27 15:46:15 +0900

    feat: 글 목록 dao 및 Service 기능

--- src/main/java/model/BoardDaoImpl.java
+++ src/main/java/model/BoardDaoImpl.java
@@ -2,7 +2,9 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import util.DBUtil;
 
@@ -15,8 +17,33 @@ public class BoardDaoImpl implements iBoardDao {
 
 	@Override
 	public List<BoardDto> selectAllBoard() {
-		// TODO Auto-generated method stub
-		return null;
+		List<BoardDto> dtos = new ArrayList<>();
+		StringBuilder sql = new StringBuilder();
+		sql.append(" select * from board ");
+		
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		try {
+			conn = dbUtil.getConnection();
+			psmt = conn.prepareStatement(sql.toString());
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				int j = 1;
+				BoardDto dto = new BoardDto(
+						rs.getInt(j++), 
+						rs.getString(j++),
+						rs.getString(j++),
+						rs.getString(j++),
+						rs.getInt(j++));
+				dtos.add(dto);
+			}
+		} catch (SQLException e) {
+			System.out.println("[ERROR] attraction exception : " + e);
+		} finally {
+			dbUtil.close(rs, psmt, conn);
+		}
+		return dtos;
 	}
 
 	@Override
